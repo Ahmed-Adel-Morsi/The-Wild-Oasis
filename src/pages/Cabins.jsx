81110ed1f0b4b2ede3e8d39c3ec682d05b02c163
Ahmed Adel <ahmedadel0239@gmail.com>
2026-04-05 18:12:16 +0200

    feat: integrate React Query for cabin management and enhance UI components
    - Added @tanstack/react-query and @tanstack/react-query-devtools for data fetching and state management.
    - Updated package.json and package-lock.json to include new dependencies.
    - Refactored App component to include QueryClientProvider and ReactQueryDevtools.
    - Created CabinTable and CabinRow components to display cabin data with loading state.
    - Implemented CreateCabinForm for adding and editing cabins with form validation using react-hook-form.
    - Added useCabins, useCreateCabin, useEditCabin, and useDeleteCabin hooks for managing cabin data.
    - Enhanced UI with styled components and improved layout in AppLayout and FormRow.
    - Added Spinner component for loading states and Toaster for notifications.
    - Updated API service functions to handle cabin creation, editing, and deletion with Supabase.

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,20 +1,29 @@
-import { useEffect } from "react";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
-import { getCabins } from "../services/apiCabins";
+import CabinTable from "../features/cabins/CabinTable";
+import { useState } from "react";
+import Button from "../ui/Button";
+import CreateCabinForm from "../features/cabins/CreateCabinForm";
 
 function Cabins() {
-  useEffect(() => {
-    getCabins().then((data) => console.log(data));
-  }, []);
+  const [showForm, setShowForm] = useState(false);
 
   return (
-    <Row type="horizontal">
-      <Heading as="h1">All cabins</Heading>
-      <p>TEST</p>
+    <>
+      <Row type="horizontal">
+        <Heading as="h1">All cabins</Heading>
+        <p>Filter / Sort</p>
+      </Row>
 
-      <img src="https://oemqyabsbiyyrpdjzyte.supabase.co/storage/v1/object/public/cabin-images/cabin-001.jpg" alt="" srcset="" />
-    </Row>
+      <Row>
+        <CabinTable />
+        <Button onClick={() => setShowForm((show) => !show)}>
+          Add new cabin
+        </Button>
+
+        {showForm && <CreateCabinForm />}
+      </Row>
+    </>
   );
 }
 
--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -1,4 +1,4 @@
-import supabase from "./supabase";
+import supabase, { supabaseUrl } from "./supabase";
 
 export async function getCabins() {
   const { data, error } = await supabase.from("cabins").select("*");
@@ -6,6 +6,58 @@ export async function getCabins() {
   if (error) {
     console.error(error);
     throw new Error("Cabins could not be loaded");
+  }
+
+  return data;
+}
+
+export async function createEditCabin(newCabin, id) {
+  const hasImagePath = newCabin.image?.startsWith?.(supabaseUrl);
+
+  const imageName = `${crypto.randomUUID()}-${newCabin.image.name}`.replaceAll(
+    "/",
+    "",
+  );
+  const imagePath = hasImagePath
+    ? newCabin.image
+    : `${supabaseUrl}/storage/v1/object/public/cabin-images/${imageName}`;
+
+  // 1. Upload image
+  if (!hasImagePath) {
+    const { error: storageError } = await supabase.storage
+      .from("cabin-images")
+      .upload(imageName, newCabin.image);
+
+    if (storageError) {
+      console.error(storageError);
+      throw new Error("Image upload failed. Cabin was not saved.");
+    }
+  }
+
+  // 2. Create/edit cabin
+  const cabinData = { ...newCabin, image: imagePath };
+
+  let query = supabase.from("cabins");
+  if (!id)
+    query = query.insert([cabinData]); // create
+  else query = query.update(cabinData).eq("id", id); // edit
+
+  const { data, error } = await query.select().single();
+
+  if (error) {
+    console.error(error);
+    throw new Error(`Cabin could not be ${id ? "Updated" : "Created"}`);
+  }
+
+  return data;
+}
+
+export async function deleteCabin(id) {
+  const { error, data } = await supabase.from("cabins").delete().eq("id", id);
+
+  if (error) {
+    console.error(error);
+    throw new Error("Cabins could not be deleted");
   }
 
   return data;
